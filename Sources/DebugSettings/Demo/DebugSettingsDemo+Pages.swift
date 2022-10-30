28b949a28e56da83f190ce23a8cfc4781edd80f8
wangzhizhou <wangzhizhou@bytedance.com>
2022-10-30 17:46:05 +0800

    拆分Demo代码逻辑，更清晰一些

--- Sources/DebugSettings/Demo/DebugSettingsDemo+Pages.swift
+++ Sources/DebugSettings/Demo/DebugSettingsDemo+Pages.swift
@@ -102,50 +102,7 @@ public class DebugSettingsDemo: ObjcBridgeClass {
         }
         return SettingsPage(pageModel: model)
     }
+    
 }
 
-// MARK: 配置项事件处理
-extension DebugSettingsDemo {
-    /// 处理设置项开关类处理事件
-    /// - Parameters:
-    ///   - entryItem: 设置项数据模型
-    ///   - isOn: 开关是否打开
-    ///   - type: 值变化的类型
-    static func switchValueChangeAction(_ entryItem: SettingsEntryItem, _ isOn: Bool, _ type: SettingsEntrySwitchActionType) {
-        
-        let mainPageIdSection1 = SettingsIdentifier(segs: [SettingsPageId.main.rawValue]).append(SettingsSectionId.section1.rawValue)
-        switch entryItem.id {
-        case mainPageIdSection1.append(SettingsEntrySwitchId.switch1.rawValue):
-            print("id: \(entryItem.id), isOn: \(isOn), type: \(type)")
-        default:
-            print("other switch change")
-        }
-    }
-    
-    
-    /// 处理按钮类设置项的事件处理器
-    /// - Parameter entryItem: 设置项数据模型
-    static func buttonClickAction(_ entryItem: SettingsEntryItem) {
-        let mainPageIdSection1 = SettingsIdentifier(segs: [SettingsPageId.main.rawValue]).append(SettingsSectionId.section1.rawValue)
-        switch entryItem.id {
-        case mainPageIdSection1.append(SettingsEntryButtonId.button1.rawValue):
-            print("id: \(entryItem.id) action")
-        default:
-            print("other button action")
-        }
-    }
-    
-    
-    /// 处理跳转子页面事件
-    /// - Parameter entryItem: 设置项数据模型
-    static func subpageJumpAction(_ entryItem: SettingsEntryItem, _ from: UIViewController) {
-        let mainPageIdSection1 = SettingsIdentifier(segs: [SettingsPageId.main.rawValue]).append(SettingsSectionId.section1.rawValue)
-        switch entryItem.id {
-        case mainPageIdSection1.append(SettingsEntrySubPageId.page1.rawValue):
-            print("id: \(entryItem.id) subpage jump")
-            from.navigationController?.pushViewController(subpage(), animated: true)
-        default:
-            print("other subpage jump")
-        }
-    }
-}
+
