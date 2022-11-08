2feadc58a9b7449e0d111b19c8b2e43199771c9b
wangzhizhou <824219521@qq.com>
2022-11-09 00:30:43 +0800

    add gitignore

---
+++ Demo/Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by joker on 2022/11/9.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
