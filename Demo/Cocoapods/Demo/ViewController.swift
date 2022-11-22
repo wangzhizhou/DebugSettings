406fab5ca79698c0513e9beee1ba34aaf4048a74
wangzhizhou <wangzhizhou@bytedance.com>
2022-11-22 12:17:04 +0800

    添加cocoapods demo

--- Demo/Cocoapods/Demo/ViewController.swift
+++ Demo/Cocoapods/Demo/ViewController.swift
@@ -6,8 +6,13 @@
 //
 
 import UIKit
+import DebugSettings
 
 class ViewController: UIViewController {
 
+    @IBAction func showDebugTools(_ sender: UIBarButtonItem) {
+        
+        DebugSettingsDemo.mainPage().pushOnTopViewController()
+    }
 }
 
