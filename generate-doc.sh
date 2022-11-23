f512483f0a0e2e534754fe6990b75676503e6df6
wangzhizhou <wangzhizhou@bytedance.com>
2022-11-23 22:16:51 +0800

    使用xcbuild docbuild 生成文档
    参考: https://github.com/apple/swift-docc-plugin/issues/29#issuecomment-1312576261

--- generate-doc.sh
+++ generate-doc.sh
@@ -1,14 +1,18 @@
 # !/usr/bin/env bash
 # -*- coding: utf-8 -*-
 
-swift package \
--Xswiftc "-sdk" \
--Xswiftc "`xcrun --sdk iphonesimulator --show-sdk-path`" \
--Xswiftc "-target" \
--Xswiftc "x86_64-apple-ios13.0-simulator" \
---allow-writing-to-directory ./docs \
-generate-documentation --target DebugSettings \
---disable-indexing \
---transform-for-static-hosting \
---hosting-base-path DebugSettings \
---output-path ./docs
+# swift package \
+# -Xswiftc "-sdk" \
+# -Xswiftc "`xcrun --sdk iphonesimulator --show-sdk-path`" \
+# -Xswiftc "-target" \
+# -Xswiftc "x86_64-apple-ios13.0-simulator" \
+# --allow-writing-to-directory ./docs \
+# generate-documentation --target DebugSettings \
+# --disable-indexing \
+# --transform-for-static-hosting \
+# --hosting-base-path DebugSettings \
+# --output-path ./docs
+
+xcodebuild docbuild -scheme DebugSettings \
+    -destination generic/platform=iphoneos \
+    OTHER_DOCC_FLAGS="--transform-for-static-hosting --hosting-base-path DebugSettings --output-path ./docs"
