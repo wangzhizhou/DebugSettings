b6d12510f5ef618f18ca801233a958d198f9cc73
wangzhizhou <wangzhizhou@bytedance.com>
2022-11-23 20:24:55 +0800

    添加SwiftDocCPlugin

--- Package.swift
+++ Package.swift
@@ -18,6 +18,8 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/SnapKit/SnapKit.git", from: "5.6.0"),
         .package(url: "https://github.com/scalessec/Toast-Swift.git", from: "5.0.1"),
+        // [SwiftDocCPlugin](https://apple.github.io/swift-docc-plugin/documentation/swiftdoccplugin/)
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
