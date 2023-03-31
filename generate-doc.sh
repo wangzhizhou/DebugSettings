94c19ce664e2ca4d772c7f673699b336d286076f
wangzhizhou <wangzhizhou@bytedance.com>
2023-03-31 17:44:19 +0800

    refine generate-doc.sh

--- generate-doc.sh
+++ generate-doc.sh
@@ -1,23 +1,33 @@
 # !/usr/bin/env bash
 # -*- coding: utf-8 -*-
 
-# swift package \
-# --disable-sandbox \
-# -Xswiftc "-sdk" \
-# -Xswiftc "`xcrun --sdk iphonesimulator --show-sdk-path`" \
-# -Xswiftc "-target" \
-# -Xswiftc "arm64-apple-ios13.0-simulator" \
-# --allow-writing-to-directory ./docs \
-# generate-documentation --target DebugSettings \
-# --disable-indexing \
-# --transform-for-static-hosting \
-# --hosting-base-path DebugSettings \
-# --output-path ./docs
+set -eu
 
-# SwiftDocCPlugin 1.0.0版本有bug，无法生成Documentation目录，临时替换方案，
-# 使用Xcodebuild docbuild编译文档
-# 详情可移步链接：https://github.com/apple/swift-docc-plugin/issues/29
+# Use git worktree to checkout the gh-pages branch of this repository in a gh-pages sub-directory
+git fetch
+git worktree add --checkout gh-pages origin/gh-pages
+
 xcodebuild docbuild -scheme DebugSettings \
     -destination generic/platform=iphoneos \
     OTHER_DOCC_FLAGS="--transform-for-static-hosting \
     --hosting-base-path DebugSettings --output-path ./docs"
+
+# Save the current commit we've just built documentation from in a variable
+CURRENT_COMMIT_HASH=`git rev-parse --short HEAD`
+
+# Commit and push our changes to the gh-pages branch
+cd gh-pages
+git add docs
+
+if [ -n "$(git status --porcelain)" ]; then
+    echo "Documentation changes found. Commiting the changes to the 'gh-pages' branch and pushing to origin."
+    git commit -m "Update GitHub Pages documentation site to '$CURRENT_COMMIT_HASH'."
+    git push origin HEAD:gh-pages
+else
+  # No changes found, nothing to commit.
+  echo "No documentation changes found."
+fi
+
+# Delete the git worktree we created
+cd ..
+git worktree remove gh-pages
